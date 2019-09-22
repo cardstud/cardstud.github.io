aab4f63bca1b91eb027b5965d6fe2d95c06eea70
cardstud <48735745+cardstud@users.noreply.github.com>
2019-09-22 13:52:34 -0700

    Create 2019-9-21-daily-show.m

---
+++ _posts/2019-9-21-daily-show.m
@@ -0,0 +1,9 @@
+---
+title: The Golden Knights are top of the league
+subtitle: Just working on learning
+image:
+---
+
+Dansk will make a road start versus the Kings on Thursday, Ben Gotz of the Las Vegas Review-Journal reports.
+
+The Golden Knights sent Dansk between the pipes for just four games last season, but the Swede skated off with three wins to complement a robust 1.78 GAA and .946 save percentage. He's a quality insurance option behind NHL regulars Marc-Andre Fleury and Malcolm Subban, but newcomer Garret Sparks and 20-year-old prospect Dylan Ferguson figure to give him a run for his money in the preseason.
